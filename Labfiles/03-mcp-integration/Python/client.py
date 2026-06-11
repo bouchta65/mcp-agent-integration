82ccd37e360cda0c2d7c9210f69da4a12c686150
bouchta65 <bouchtamohamed01@gmail.com>
2026-06-11 11:27:17 +0100

    feat: map mcp tools into agent functions

--- Labfiles/03-mcp-integration/Python/client.py
+++ Labfiles/03-mcp-integration/Python/client.py
@@ -39,3 +39,41 @@ async def connect_to_server(exit_stack: AsyncExitStack):
     # List available tools
     response = await session.list_tools()
     tools = response.tools
+    print("\nConnected to server with tools:", [tool.name for tool in tools])    
+
+    return session
+
+async def chat_loop(session):
+
+    # Connect to the agents client
+    with (
+        DefaultAzureCredential() as credential,
+        AIProjectClient(endpoint=project_endpoint, credential=credential) as project_client,
+        project_client.get_openai_client() as openai_client,
+    ):
+
+        # Get the mcp tools available from the server
+        response = await session.list_tools()
+        tools = response.tools
+
+        # Build a function for each tool
+        def make_tool_func(tool_name):
+            async def tool_func(**kwargs):
+                result = await session.call_tool(tool_name, kwargs)
+                return result
+                
+            tool_func.__name__ = tool_name
+            return tool_func
+
+        # Store the functions in a dictionary for easy access when processing function calls
+        functions_dict = {tool.name: make_tool_func(tool.name) for tool in tools}
+
+        # Create FunctionTool definitions for the agent
+        mcp_function_tools: FunctionTool = []
+        for tool in tools:
+            function_tool = FunctionTool(
+                name=tool.name,
+                description=tool.description,
+                parameters={
+                    "type": "object",
+                    "properties": {},
